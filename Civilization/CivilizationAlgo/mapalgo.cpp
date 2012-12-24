9226b6bc3098f501d81dc56fd9a4da76b89d0394
Amandine <aceltiss@hotmail.com>
2012-12-24 17:22:30 +0100

    clean

--- Civilization/CivilizationAlgo/mapalgo.cpp
+++
@@ -1,9 +0,0 @@
-#include "mapalgo.h"
-
-int Algo::computeFoo() {
-	return 1;
-}
-
-Algo* Algo_new() { return new Algo(); }
-void Algo_delete(Algo* algo) { delete algo; }
-int Algo_computeAlgo(Algo* algo) { return algo->computeFoo(); } 
