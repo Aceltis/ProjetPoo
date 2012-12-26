8cd43b00b7b5284e09f1ab3e432afdbc4303997d
Amandine <aceltiss@hotmail.com>
2012-12-26 09:36:58 +0100

    mapalgo

--- Civilization/CivilizationAlgo/mapalgo.cpp
+++ Civilization/CivilizationAlgo/mapalgo.cpp
@@ -1,4 +1,7 @@
+#include <time.h>
 #include "mapalgo.h"
+
+using namespace std;
 
 int Algo::computeFoo() {
 	return 1;
@@ -6,4 +9,40 @@ int Algo::computeFoo() {
 
 Algo* Algo_new() { return new Algo(); }
 void Algo_delete(Algo* algo) { delete algo; }
-int Algo_computeAlgo(Algo* algo) { return algo->computeFoo(); } 
+int Algo_computeAlgo(Algo* algo) { return algo->computeFoo(); }
+
+/*
+ * createSmallMap()
+ * Create a new 25x25 map
+ * return vector<vector<square>>
+ */
+vector<vector<square>> createSmallMap() {
+	int height, width = 25;
+	int rand, bonus;
+	vector<vector<square>> smallMap;
+	
+	// Initialize random seed
+	srand(time(NULL));
+
+	// Set up sizes. (height x width)
+	smallMap.resize(height);
+	for (int i = 0; i < height; ++i)
+		smallMap[i].resize(width);
+
+	// Free random on the first square
+	rand = rand() % 3 + 1;
+	smallMap[0][0].type = rand;
+	bonus = rand() % 3 + 1;
+	smallMap[0][0].bonus = bonus;
+
+	for(int i = 0; i<25; i++)
+	{
+		for(int j = 0; j<25; j++)
+		{
+			
+			rand = rand() % 3 + 1;
+			smallMap[i][j].type;
+		}
+	}
+
+}
--- Civilization/CivilizationAlgo/mapalgo.h
+++ Civilization/CivilizationAlgo/mapalgo.h
@@ -6,6 +6,17 @@
 #define EXTERNC
 #endif
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+struct square {
+	// 1: Mountain, 2: Plain, 3: Desert
+	int type;
+	// 1: No additionnal ressource, 2: Additionnal Iron, 3: Additionnal Food
+	int bonus;
+};
+
 class DLL Algo {
 	public:
 		Algo() {}
@@ -17,3 +28,4 @@ class DLL Algo {
 EXTERNC DLL Algo* Algo_new();
 EXTERNC DLL void Algo_delete(Algo* algo);
 EXTERNC DLL int Algo_computeAlgo(Algo* algo);
+EXTERNC DLL vector<vector<square>> createSmallMap();
